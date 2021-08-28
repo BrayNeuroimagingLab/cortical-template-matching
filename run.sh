c1f8107128cc9e7be6c52c5804c14cbab48de2bb
rylan.marianchuk <rylanmarianchuk@gmail.com>
2021-08-27 18:21:38 -0600

    Added the slurm execution script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --time=01:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --partition=cpu2019
+#SBATCH --mem=120G
+#SBATCH --mail-user=rylan.marianchuk@ucalgary.ca
+#SBATCH --mail-type=ALL
+#SBATCH --output="outfile.out"
+
+python groupwiseTemplate.py $file
+
+
